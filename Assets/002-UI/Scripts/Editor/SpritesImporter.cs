eda2643418c5ca4685f09e39fc377d6d3ef9faaf
Serhii Chechui <Serhii.Chechui@dgngames.com>
2019-05-30 18:16:06 +0300

    Initial commit.

---
+++ Assets/002-UI/Scripts/Editor/SpritesImporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SpritesImporter : AssetPostprocessor
+{
+    void OnPreprocessTexture() {
+        
+        var textureImporter = (TextureImporter) assetImporter;
+
+        if (!textureImporter.assetPath.Contains("Sprites")) return;
+        textureImporter.textureCompression = TextureImporterCompression.CompressedHQ;
+        textureImporter.textureType = TextureImporterType.Sprite;
+        textureImporter.spriteImportMode = SpriteImportMode.Single;
+    }
+}
